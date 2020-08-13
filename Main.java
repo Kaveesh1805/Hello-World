4365923f398ef7eaaa4410d8eb58b29b5141e5dc
Kaveesh1805 <62790657+Kaveesh1805@users.noreply.github.com>
2020-08-12 19:36:10 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("I am Kaveesh Passari");
+		System.out.println("I like to play soccer");
+		System.out.println("My favourite game is Red Dead Redemption 2");
+		System.out.println("I want to major in computer science");
+		System.out.println("I am aiming for colleges such as UC Berkley and Stanford");
+	}
+}
